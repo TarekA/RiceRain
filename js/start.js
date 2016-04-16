058744c3b5650d6f1e3e137dd7d3d3da8587f6af
Zedd <t.abdel-aziz@gmx.at>
2016-04-16 15:46:19 +0200

    start.js wiederhergestellt

--- js/start.js
+++ js/start.js
@@ -4,23 +4,20 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-    game.load.image('dish', 'assets/dish.png');
-    game.load.spritesheet('grain', 'assets/grain.png');
+    game.load.image('atari', 'assets/dish.png');
     game.load.physics("sprite_physics", "assets/exported_shapes.json");
-    //game.load.spritesheet('bullets', 'assets/grain.png', 17, 17);
+    game.load.image('background', 'assets/example/rice_copy.png');
+    game.load.spritesheet('button', 'assets/example/button_sprite_sheet.png', 193, 71);
+    game.load.spritesheet('grain', 'assets/grain.png');
+    game.load.image('dish', 'assets/dish.png');
 }
 
 function create(){
-    //var game = new Phaser.Game(320, 480, Phaser.CANVAS, "game");
-    //game.state.add("Start",start);
-    //game.state.add("TitleScreen",titleScreen);
+    game.state.add("TitleScreen",titleScreen);
     game.state.add("GameState",gameState);
-
 }
 function update(){
-    console.log("Start");
-    game.state.start("GameState");
-    console.log("Ende");
+    game.state.start("TitleScreen");
 }
 
 function render() {
