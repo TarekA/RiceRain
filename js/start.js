39bc3855c82893d3bbae03189d45351ea83947c7
Zedd <t.abdel-aziz@gmx.at>
2016-04-16 15:49:22 +0200

    titleScreen Bilder ausgetauscht

--- js/start.js
+++ js/start.js
@@ -5,9 +5,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
     game.load.image('atari', 'assets/dish.png');
+    game.load.spritesheet('background', 'assets/spritesheets/titlescreen-rice.png');
+    game.load.spritesheet('button', 'assets/spritesheets/startbutton-sprite-x2.png', 200, 40);
+    game.load.spritesheet('bullets', 'assets/grain.png', 17, 17);
     game.load.physics("sprite_physics", "assets/exported_shapes.json");
-    game.load.image('background', 'assets/example/rice_copy.png');
-    game.load.spritesheet('button', 'assets/example/button_sprite_sheet.png', 193, 71);
     game.load.spritesheet('grain', 'assets/grain.png');
     game.load.image('dish', 'assets/dish.png');
 }
